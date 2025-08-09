64e3db867771b022be39ed6b2a9254b7d668b7d2
Siddharth Bahl <bahlsiddharth123@gmail.com>
2025-08-09 16:47:18 +0100

    completing the second project

--- second_react/src/App.jsx
+++ second_react/src/App.jsx
@@ -1,32 +1,20 @@
 import HeaderComponent from "./components/HeaderComponent.jsx";
 import EntryComponent from "./components/EntryComponent.jsx";
 import placesData from './LocationData.js';
-import React from "react";
-
-function GetComponentsWithProps(){
-    let components = [];
-
-    for (const element in placesData){
-        let ComponentInstance = <EntryComponent />;
-
-        for (const [keyName, valueName] of Object.entries(placesData[element])){
-            let data = {};
-            data[keyName] = valueName;
-
-            ComponentInstance = React.cloneElement(ComponentInstance, data);
-        }
-        
-        console.log(ComponentInstance);
-        components.push(ComponentInstance);
-    }
-    return components;
-}
 
 export default function App(){
+    const placesArray = placesData.map((entry) => {
+        return (
+            <EntryComponent 
+                {...entry}
+            />
+        );
+    })
+
     return (
         <>
             <HeaderComponent />
-            {GetComponentsWithProps()}
+            {placesArray}
         </>
     );
 };
